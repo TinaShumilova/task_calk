dbf377fdcf0c75be7fe842af7910c45b83688219
Tina Shumilova <shutiva@gmail.com>
2022-02-14 19:58:44 +0200

    стадия 3

--- controller.py
+++ controller.py
@@ -1,2 +1,9 @@
-def button_click():
+import csv_generator as gen
+import csv_reader as red
+
+
+def write_file():
+    return 0
+
+def read_file():
     return 0
--- user_interface.py
+++ user_interface.py
@@ -1,6 +1,17 @@
 def get_name():
     return input()
 
+def get_last_name():
+    return 0
+
+def get_phone():
+    return 0
+
+def get_comment():
+    return 0
 
 def choose_format():
+    return 0
+
+def export_console():
     return 0
