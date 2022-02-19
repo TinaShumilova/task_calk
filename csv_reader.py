6cf8fc54e59fcd3a49ded7ed707971f15876cd66
RenatBax <renchik_125.111@mail.ru>
2022-02-19 21:26:49 +0300

    Изменил reader, ветка new_reader

--- csv_reader.py
+++ csv_reader.py
@@ -1,12 +1,20 @@
-#from csv_generator import data
-from user_interface import export_console
+from user_interface import export_console, export_console_2
+
 data = 'file.csv'
 
-def read_csv():
-    with open(data, 'r') as rd:
-        # rd.readlines()
-        for i in rd:
-            export_console(i.split(';'))
-            #print(i)
+def read_csv(version = 1): # Не знаю как бyдет организация выбора строчек и столбцов
+    if version == 1:    
+        with open(data, 'r') as dt:
+            for item in dt:
+                export_console(item.split(';'))  # строка, но для этого в ui в этом методе надо поменять вывод
+    else: 
+        with open(data, 'r') as dt: # столбец
+            for item in dt:
+                line_dt = item.split(';')
+                dic = {
+                    'Имя': line_dt[0],
+                    'Фамилия': line_dt[1],
+                    'Телефон' : line_dt[2],
+                    'Коментарий': line_dt[3]}
+                export_console_2(dic)
         
-#read_csv(data)
